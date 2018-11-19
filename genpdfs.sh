a17cbfb9a0c4b43f332b78506d2de828c717758e
Henrik Berglund <henrik.berglund.se@gmail.com>
2018-11-19 22:48:10 +0100

    Added singe filed to genpdfs scripts and updated everything

--- genpdfs.sh
+++ genpdfs.sh
@@ -1,16 +1,30 @@
 
+
 pandoc \
-Team-Start-Example.md \
-Ground-Rules-and-Decision-Making.md \
-Journey-Lines-Facilitation-Guide.md \
-Appreciation-Cards.md \
-Product-Goals-for-Teams.md \
-Organizational-Goals-for-Teams.md \
-Team-Vision.md \
-Individual-Goals.md \
-Balancing-Team-and-Individual-work.md \
-Market-of-skills.md \
-Repairing-Broken-Agreements.md \
-Build-trust-with-simple-questions.md \
--o test.pdf -H style/footer.tex --toc --toc-depth=1 --top-level-division=chapter -V secnumdepth=0
+guides/Team-Start-Example.md \
+guides/Ground-Rules-and-Decision-Making.md \
+guides/Journey-Lines-Facilitation-Guide.md \
+guides/Appreciation-Cards.md \
+guides/Product-Goals-for-Teams.md \
+guides/Organizational-Goals-for-Teams.md \
+guides/Team-Vision.md \
+guides/Individual-Goals.md \
+guides/Balancing-Team-and-Individual-work.md \
+guides/Market-of-skills.md \
+guides/Repairing-Broken-Agreements.md \
+guides/Build-trust-with-simple-questions.md \
+-o pdf/Starting-and-Developing-Agile-Teams.pdf -H guides/style/footer.tex --toc --toc-depth=1 --top-level-division=chapter -V secnumdepth=0
+
+pandoc guides/Team-Start-Example.md -o pdf/Team-Start-Example.pdf -H guides/style/footer.tex
+pandoc guides/Ground-Rules-and-Decision-Making.md -o pdf/Ground-Rules-and-Decision-Making.pdf -H guides/style/footer.tex
+pandoc guides/Journey-Lines-Facilitation-Guide.md -o pdf/Journey-Lines-Facilitation-Guide.pdf -H guides/style/footer.tex
+pandoc guides/Appreciation-Cards.md -o pdf/Appreciation-Cards.pdf -H guides/style/footer.tex
+pandoc guides/Product-Goals-for-Teams.md -o pdf/Product-Goals-for-Teams.pdf -H guides/style/footer.tex
+pandoc guides/Organizational-Goals-for-Teams.md -o pdf/Organizational-Goals-for-Teams.pdf -H guides/style/footer.tex
+pandoc guides/Team-Vision.md -o pdf/Team-Vision.pdf -H guides/style/footer.tex
+pandoc guides/Individual-Goals.md -o pdf/Individual-Goals.pdf -H guides/style/footer.tex
+pandoc guides/Balancing-Team-and-Individual-work.md -o pdf/Balancing-Team-and-Individual-work.pdf -H guides/style/footer.tex
+pandoc guides/Market-of-skills.md -o pdf/Market-of-skills.pdf -H guides/style/footer.tex
+pandoc guides/Repairing-Broken-Agreements.md -o pdf/Repairing-Broken-Agreements.pdf -H guides/style/footer.tex
+pandoc guides/Build-trust-with-simple-questions.md -o pdf/Build-trust-with-simple-questions.pdf -H guides/style/footer.tex
 
