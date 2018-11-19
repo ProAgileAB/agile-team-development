b0abafa7c326f289babb29a749f90b99c9bf1aa7
Henrik Berglund <henrik.berglund@proagile.se>
2018-11-19 21:44:21 +0100

    Added script to generate full pdfs

---
+++ chapter_file/genpdfs.sh
@@ -0,0 +1,16 @@
+
+pandoc \
+Team-Start-Example.md \
+Ground-Rules-and-Decision-Making.md \
+Journey-Lines-Facilitation-Guide.md \
+Appreciation-Cards.md \
+Product-Goals-for-Teams.md \
+Organizational-Goals-for-Teams.md \
+Team-Vision.md \
+Individual-Goals.md \
+Balancing-Team-and-Individual-work.md \
+Market-of-skills.md \
+Repairing-Broken-Agreements.md \
+Build-trust-with-simple-questions.md \
+-o test.pdf -H style/footer.tex --toc --toc-depth=1 --top-level-division=chapter -V secnumdepth=0
+
